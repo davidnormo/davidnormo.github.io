f85f2dd9725b531022ac3bc0df4f9c81697c41b8
David Normington <david.normington@arcadiagroup.co.uk>
2018-04-17 13:52:21 +0100

    Changes

--- gen.sh
+++ gen.sh
@@ -11,44 +11,51 @@ getFilename="sed -E 's/markdown\/(.*).md$/\1/'"
 mdToHTML="marked --gfm"
 
 function genPost {
-	file=$1
-	sha=$2
-	filename=$(echo $file | eval $getFilename)
-	mkdir -p "posts/$sha"
-	echo "Creating posts/$sha/$filename.html"
-	# convert md to html
-	content=$(git show $sha:$file | eval $mdToHTML)
-	# add slashes so it can be used in sed replace
-	content="$(quoteSubst "$content")"
-	echo $postTemplate | sed "s/{{content}}/$content/" > "posts/$sha/$filename.html"
+  file=$1
+  sha=$2
+  filename=$(echo $file | eval $getFilename)
+  mkdir -p "posts/$sha"
+  echo "Creating posts/$sha/$filename.html"
+  # convert md to html
+  content=$(git show $sha:$file | eval $mdToHTML)
+  # add slashes so it can be used in sed replace
+  content="$(quoteSubst "$content")"
+  cat template.html | sed -e ':a' -e '$!{N;ba' -e '}' -e "s/{{content}}/$content/" > "posts/$sha/$filename.html"
 }
 
 # WIP
 function genDiff {
-	file=$1
-	sha=$2
-	filename=$(echo $file | eval $getFilename)
-	mkdir -p "posts/$sha/$filename"
-	content=$(git diff --no-color $sha $file | eval $mdToHTML)
-	content="$(quoteSubst "$content")"
-	content=$(echo $postTemplate | sed -e ':a' -e '$!{N;ba' -e '}' -e "s/{{content}}/$content/")
-	echo $content > "posts/$sha/$filename/diff.html"
+  file=$1
+  sha=$2
+  filename=$(echo $file | eval $getFilename)
+  mkdir -p "posts/$sha/$filename"
+  content=$(git diff --color $sha $file | ./ansi2html.sh --body-only)
+  content="$(quoteSubst "$content")"
+  content=$(cat template.html | sed -e ':a' \
+  -e '$!{N;ba' \
+  -e '}' \
+  -e "s/{{content}}/$content/")
+  echo $content > "posts/$sha/$filename/diff.html"
 }
 
-files=$(find markdown -name *.md)
-postTemplate=$(cat template.html)
+files=$(find markdown -name "*.md")
 
+echo $files
 # build posts only for markdown files that exist on HEAD
 for file in $files; do
-	for sha in $(git log --format=%h $file); do
-		genPost $file $sha
-	done
+  for sha in $(git log --format=%h $file); do
+    genPost $file $sha
+    genDiff $file $sha
+  done
 done
 
 for file in $files; do
-	genPost $file "head"
+  genPost $file "head"
 done
+
+content=$(cat index.md | eval $mdToHTML)
+content="$(quoteSubst "$content")"
+cat template.html | sed -e ':a' -e '$!{N;ba' -e '}' -e "s/{{content}}/$content/" > "index.html"
 
 echo "All done!"
 tree posts
-
---
+++ index.md
@@ -0,0 +1,4 @@
+# Posts
+
+ - [Ecom Development Fact base](/posts/head/ecom-fact-base.html)
+ - [Reuse logic, avoid duplication, maintain performance with transducers](/posts/head/transducers.html)
