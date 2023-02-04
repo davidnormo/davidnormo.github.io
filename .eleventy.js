2616890ea43e52b6cf9628a60fd7f9fe4bc299fc
Dave Normington <david.normo@gmail.com>
2023-02-04 11:52:52 +0000

    hopefully fixing

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  return {
+    dir: {
+      input: ".",
+      output: "public",
+    },
+  };
 };
