e08b65bc61db0f1e34b323e5dcd43ac0206e1fa8
Dave Normington <dave.normington@troweprice.com>
2023-02-07 10:30:28 +0000

    bits and bobs

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,34 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
+const isDevEnv = process.env.ELEVENTY_ENV === 'development';
+
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(syntaxHighlight);
 
+function showDraft(data) {
+	const isDraft = 'draft' in data && data.draft !== false;
+	return isDevEnv || !isDraft;
+}
+
   return {
+		eleventyComputed: {
+			eleventyExcludeFromCollections: function(data) {
+				if(showDraft(data)) {
+					return data.eleventyExcludeFromCollections;
+				}
+				else {
+					return true;
+				}
+			},
+			permalink: function(data) {
+				if(showDraft(data)) {
+					return data.permalink
+				}
+				else {
+					return false;
+				}
+			}
+		},
     dir: {
       input: ".",
       output: "docs",
